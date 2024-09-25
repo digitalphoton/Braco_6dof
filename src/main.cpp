6fc6d0ea97d115a115a39daf66088303e85c8d9a
João Vitor S. Anacleto <22249746+digitalphoton@users.noreply.github.com>
2024-09-25 19:23:17 -0300

    Changed initialization routine

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,27 @@ Braco braco;
 
 void setup()
 {
+  Serial.begin(115200);
+  while(!Serial);
+  delay(1000);
+
+  Serial.println(
+    "Favor ligar fonte externa. Aperte Enter para continuar."
+  );
+
+  // Esperar entrada, e depois descartar os bytes recebidos
+  while(!Serial.available());
+  while(Serial.available() > 0)
+  {
+    Serial.read();
+  }
+
   braco.init();
+
   delay(2000);
+  Serial.println("Braço inicializado!");
+  delay(5000);
+  
   braco.setOmbro(70.0);
   braco.setCotovelo(0.0);
   braco.setPulsoFlexao(-70.0);
