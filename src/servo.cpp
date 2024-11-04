3d4ea2834be8d6edf1783d629a17a3a839f0573f
João Vitor S. Anacleto <22249746+digitalphoton@users.noreply.github.com>
2024-11-04 11:25:59 -0300

    Separated update logic to be implemented in main; added limits to movement

--- src/servo.cpp
+++ src/servo.cpp
@@ -1,20 +1,38 @@
 #include "servo.h"
 
-Servo::Servo(uint8_t newPin, uint8_t newChannel)
+Servo::Servo(
+	uint8_t newPin,
+	uint8_t newChannel,
+	float newMinPosDegree,
+	float newMaxPosDegree,
+	float initPosDegree
+)
 {
 	channel = newChannel;
 	pin = newPin;
+	curPosDegree = initPosDegree;
+	minPosDegree = newMinPosDegree;
+	maxPosDegree = newMaxPosDegree;
 }
 void Servo::init(void)
 {
 	ledcAttachPin(pin, channel);
 	ledcSetup(channel, GLOBAL_PWM_FREQ, GLOBAL_PWM_RES);
+	setPosition(curPosDegree);
 }
-void Servo::setPosition(float newPosDegree)
+void Servo::setPosition(float posDegree)
+{
+	uint16_t dutyCycle = (curPosDegree+180)/2400.0 * 0xffff;
+	// O dutyCycle precisa ser invertido quando o servo é acionado via um
+	// transistor NPN
+	//dutyCycle = ~dutyCycle;
+	ledcWrite(channel, dutyCycle);
+}
+void Servo::setTargetPosition(float newPosDegree, float feedRate)
 {
 	float delta = newPosDegree - curPosDegree;
 	float stepValue = feedRate * 0.001 * UPDATE_STEP;
-	unsigned long stepCount = abs( delta / stepValue );
+	//unsigned long stepCount = abs( delta / stepValue );
 
 	if(delta < 0)
 	{
@@ -22,18 +40,31 @@ void Servo::setPosition(float newPosDegree)
 	}
 
 
+/*
 	for(stepCount; stepCount > 0; stepCount--)
 	{
 		unsigned long lastMillis = millis();
 
 		curPosDegree += stepValue;
 
-		uint16_t dutyCycle = (curPosDegree+180)/2400.0 * 0xffff;
-		// O dutyCycle precisa ser invertido quando o servo é acionado via um
-		// transistor NPN
-		//dutyCycle = ~dutyCycle;
-		ledcWrite(channel, dutyCycle);
+		setPosition(curPosDegree);
 
 		while( millis() < lastMillis + UPDATE_STEP);
+	}
+*/
+}
+void Servo::step(void)
+{
+	if(stepValue == 0.0) 
+	{
+		return;
+	}
+
+	curPosDegree += stepValue;
+	setPosition(curPosDegree);
+
+	if( abs(curPosDegree - targetPosDegree) < TOLERANCE)
+	{
+		stepValue = 0.0;
 	}
 }
--- src/servo.h
+++ src/servo.h
@@ -7,18 +7,33 @@
 #define GLOBAL_PWM_FREQ 50
 
 #define UPDATE_STEP 20
+#define TOLERANCE 0.1
 
 class Servo
 {
 	uint8_t channel;
 	uint8_t pin;
+
+	float minPosDegree;
+	float maxPosDegree;
+
 	float curPosDegree;
-	float feedRate;
+	float targetPosDegree;
+	float stepValue;
+
+	void setPosition(float posDegree);
 
 	public:
 	
-	Servo(uint8_t newPin, uint8_t newChannel);
+	Servo(
+		uint8_t newPin,
+		uint8_t newChannel,
+		float minPosDegree,
+		float maxPosDegree,
+		float initPosDegree
+	);
 	void init(void);
-	void setPosition(float posDegree);
+	void setTargetPosition(float posDegree, float feedRate);
+	void step(void);
 };
 #endif
